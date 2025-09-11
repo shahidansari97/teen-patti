907f987a2c34b58da1c483533ef466c7a4ad84c2
shahidansari97 <shahid@nextolive.com>
2025-09-11 13:53:34 +0530

    Third commit message

--- src/PattiCounter.jsx
+++ src/PattiCounter.jsx
@@ -3,7 +3,7 @@ import "./PattiCounter.css"; // Import CSS
 
 const PattiCounter = () => {
   const suits = ["♥️", "♣️", "♦️", "♠️"];
-  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "K", "Q", "J", "A"];
+  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
   // Generate all 52 cards dynamically
   const allCards = suits.flatMap((suit) =>
